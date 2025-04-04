9ed29d7d2a89ee7c77181342b230f6cf02ae7f2c
Pedro Valencia Alves <pedrovalenciaalves98@gmail.com>
2025-04-04 19:12:31 +0000

    proyecto terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  generateExcuse();
+  document.getElementById("generateButton").addEventListener("click", generateExcuse);
 };
+function generateExcuse() {
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  let excuse =
+    `${who[Math.floor(Math.random() * who.length)]} ${action[Math.floor(Math.random() * action.length)]} ${when[Math.floor(Math.random() * when.length)]}${what[Math.floor(Math.random() * what.length)]}.`;
+  document.getElementById("excuse").innerText = excuse;
+  console.log(excuse)
+}
